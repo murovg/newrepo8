daed1de3c51ea7633beeb5751cf1c8882d50f2c1
unknown <student@VT319-09.BT.local>
2025-04-28 13:44:51 +0400

    m

--- main/main/main.cpp
+++ main/main/main.cpp
@@ -2,15 +2,20 @@
 #define _STD_SECURE_NO_WARNINGS 
 #include "stdafx.h"
 #include "stdlib.h"
+#include "time.h"
 
 
 int main()
 {
+	srand(time(NULL));
 	int arr[10];
 	//от -10 до 40
 	for(int i=0; i<10; i++){
 		arr[i] = rand() % (40 -11)+10;
-		printf("%d ",arr[i]);
+		if(arr[i]%2==0){
+
+			printf("%d ",arr[i]);
+		}
 	}
 	return 0;
 }
